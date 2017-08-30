4433cd3c19b31cd668c3f7c0591ea462d68e4436
arun789 <adwiti1975@gmail.com>
2017-08-30 01:59:37 -0700

    first commit

---
+++ WebserviceJAXRS/src/main/java/com/arun/WebserviceJAXRS/resources/MessageResource.java
@@ -0,0 +1,18 @@
+package com.arun.WebserviceJAXRS.resources;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/messages")
+public class MessageResource {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getMessage(){
+		return "Hello World";
+	}
+
+}
